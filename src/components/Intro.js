83196cd7fd871b7293615629cafb33e44ddc223e
Sarah Harvey <s_harvey15@yahoo.com>
2019-10-16 12:11:46 +0100

    Learning components

---
+++ src/components/Intro.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Intro extends React.Component {
+
+render()  {
+    return (
+        <p>Welcome to my wonderful new app!</p>
+    );
+}
+
+}
+
+export default Intro;
