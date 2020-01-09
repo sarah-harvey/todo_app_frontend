384bdeb72cc18f9e90a882befd17e8d4d2c00f41
Sarah Harvey <s_harvey15@yahoo.com>
2020-01-09 20:41:12 +0000

    Updates

---
+++ precache-manifest.50de9022229061a867f5659ba8677bd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd1c45c21761bf7e0db571251f7dc51c",
+    "url": "/todo_app_frontend/index.html"
+  },
+  {
+    "revision": "823d4d8f3b11a1e7c52f",
+    "url": "/todo_app_frontend/static/css/main.e9223098.chunk.css"
+  },
+  {
+    "revision": "4b30747a31dd1aa871e5",
+    "url": "/todo_app_frontend/static/js/2.c43933e4.chunk.js"
+  },
+  {
+    "revision": "2ddb796e2cfea5f03cbea984689c4c9d",
+    "url": "/todo_app_frontend/static/js/2.c43933e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "823d4d8f3b11a1e7c52f",
+    "url": "/todo_app_frontend/static/js/main.9df1c356.chunk.js"
+  },
+  {
+    "revision": "1c835c600adf1d4f0054",
+    "url": "/todo_app_frontend/static/js/runtime-main.2fdaf3f4.js"
+  },
+  {
+    "revision": "1826050ebd812833262e7ea17a075474",
+    "url": "/todo_app_frontend/static/media/blue_wish_background.1826050e.jpg"
+  }
+]);
